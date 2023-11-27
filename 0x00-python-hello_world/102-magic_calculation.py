de1cb0671cd5edeebc9cbd70226d6bd9f1a7381a
MahniIslem <mehenniislem@yahoo.com>
2023-11-27 07:02:48 -0800

    0x00py

---
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -0,0 +1,5 @@
+def magic_calculation(a, b):
+    result = 98  # LOAD_CONST 1 (98)
+    result += a   # LOAD_FAST 0 (a)
+    result **= b  # LOAD_FAST 1 (b), BINARY_POWER
+    return result  # BINARY_ADD, RETURN_VALUE
